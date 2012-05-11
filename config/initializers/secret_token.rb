c752c8b3f2d6a88699162943a79046de504fb41f
Josiah <legendaryfox@gmail.com>
2012-05-12 01:03:44 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Collegeproject2::Application.config.secret_token = '245a65a6453a963bde5bd6862f38afbd177654e3d7f0dbc6a0aba65caa18d18c57a06661bddbba5fb92d7506b7b402e3b55a269a3906eef4a8a3854f64ac208f'
