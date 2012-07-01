63bcb97ce1ca8dc2ba23a461c06d29286390308c
Josiah <legendaryfox@gmail.com>
2012-07-02 00:48:01 +0800

    Stuff

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
   def home
+    
     # Find the city they are in
     if (!request.location.city.empty? && !request.location.state.empty? && !request.location.country.empty?)
       puts "Case 1"
@@ -11,21 +12,40 @@ class PagesController < ApplicationController
     
     @user_location = "Boston, MA, USA"
 
-    
     # Geocode their location
     user_coordinates = Geocoder.coordinates(@user_location) || [42.3584308, -71.0597732] 
     
+    
+    
+    # fategory filter
+    @categories = Cbo.category_counts
+
+    if (!params[:category] || params[:category] == '')
+      @cbos = Cbo.all
+      
+      # Nearby CBOs - 30 mile radius
+      @nearby_cbos = Cbo.near(user_coordinates, 30)
+    else
+      category_string = params[:category]
+      @cbos = Cbo.tagged_with(category_string)     
+      
+      # Nearby CBOs - 30 mile radius
+      @nearby_cbos = @cbos.near(user_coordinates, 30) #Cbo.near(user_coordinates, 30) 
+    end
+    
+    
+    
+    
     # Nearby Communities - 30 mile radius
-    puts "user coordinates is #{user_coordinates} for #{@user_location}"
+    #puts "user coordinates is #{user_coordinates} for #{@user_location}"
     @nearby_communities = Community.near(user_coordinates, 30)
     
-    # Nearby CBOs - 30 mile radius
-    @nearby_cbos = Cbo.near(user_coordinates, 30)
+    
     
     # Nearby Students - 30 mile radius
     @nearby_students = Student.near(user_coordinates, 30)
     
-    @cbos = Cbo.all
+    #@cbos = Cbo.all
     
   end
 
